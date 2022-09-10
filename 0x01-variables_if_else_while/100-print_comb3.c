342939d30def04d465816f232e818c29e909babf
Victoeriah <oyedapovictoria@gmail.com>
2022-09-10 09:03:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,21 +12,25 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c < i)
+		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(c);
-			putchar(i);
-
-			if (c != '8' || (c == '8' && i != '9'))
+			if (c < i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+
+	putchar('\n');
+
+	return (0);
 }
 
-putchar('\n');
 
-return (0);
-}
