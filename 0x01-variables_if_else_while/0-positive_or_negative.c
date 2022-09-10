2a070a2929d9f06b8789d14d00a3cb473b17b657
Victoeriah <oyedapovictoria@gmail.com>
2022-09-09 21:17:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints a ramdom number  and states whether 
- *        it is postive, negative, or zero.
+ * main - Prints if number is positive,zero or negative 
  *
- * Return: Always 0.
+ * Return: Always (Success)
  */
 int main(void)
 {
@@ -16,17 +15,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	
+	{
 		 printf("%d is positive\n", n);
-
-	else if (n < 0)
-
-		printf("%d is negative\n", n);
-	
-	else
-
+	}
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
-
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
 	return (0);
 }
