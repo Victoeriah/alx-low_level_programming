db835f67666290cb3ce859f151d0676ba6b0c97e
Victoeriah <oyedapovictoria@gmail.com>
2022-09-13 15:17:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
@@ -15,5 +17,6 @@ int main(void)
 		_putchar(str[count]);
 	}
 	_putchar('\n');
+
 	return (0);
 }
