347ee92804a61d87716cde93eec26f703814d914
Victoeriah <oyeddapovictoria@gmail.com>
2022-09-21 12:58:40 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,45 +1,27 @@
 #include "main.h"
 
 /**
- * _strlen - function that return the lenght of a string
- * @s: the string
- * Return: length of s
+ * _strncat - Concatenates one string to another
+ * @dest: Destination string
+ * @src: String to be concatenated
+ * @n: number of bytes to be copied
+ *
+ * Return: The destination string
  */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i))
-		i++;
-
-	return (i);
-}
-
-/**
- * _strncat - function that concatenates two strings
- * @dest: first string
- * @src: second string
- * @n: bytes from src the function will use
- * Return: a pointer to the resulting string dest
- */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int ls, ld;
+	int i;
+	int destlen = 0;
+	int counter = 0;
 
-	ld = _strlen(dest);
-	ls = _strlen(src) - 1;
-	if (n < ls)
-		ls = n;
-
-	while (ls >= 0)
+	while (dest[counter] != '\0')
 	{
-		*(dest + ld + ls) = *(src + ls);
-		ls--;
+		destlen++;
+		counter++;
 	}
-	*(dest + _strlen(dest)) = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[destlen + i] = src[i];
+	dest[destlen + i] = '\0';
 
 	return (dest);
 }
